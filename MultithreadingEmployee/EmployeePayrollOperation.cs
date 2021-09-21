a72c2dd6748750ca571b2a91abb4c34de46c2341
rani1029 <ranig1029@gmail.com>
2021-09-21 11:54:34 +0530

    using thread opeation done

---
+++ MultithreadingEmployee/EmployeePayrollOperation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingEmployee
+{
+    public class EmployeePayrollOperation
+    {
+        public List<EmployeeDetails> employeePayrollDetailsList = new List<EmployeeDetails>();
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDetailsList)
+        {
+            employeePayrollDetailsList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                this.addEmployeeToPayroll(employeeData);
+                Console.WriteLine("Employee added: " + employeeData.EmployeeName);
+            });
+            Console.WriteLine(this.employeePayrollDetailsList.ToString());
+
+        }
+
+        public void addEmployeeToPayrollWithThread(List<EmployeeDetails> employeePayrollDetailsList)
+        {
+
+            employeePayrollDetailsList.ForEach(employeeData =>
+            {
+                Task thread = new Task(() =>
+                {
+                    Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                    this.addEmployeeToPayroll(employeeData);
+                    Console.WriteLine("Employee added: " + employeeData.EmployeeName);
+                });
+                thread.Start();
+
+            });
+            Console.WriteLine(this.employeePayrollDetailsList.Count);
+        }
+
+        public void addEmployeeToPayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailsList.Add(emp);
+        }
+    }
+}
